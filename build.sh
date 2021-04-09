0fdd66e3cdf5c646c3a47c4bfa31dd7afdc143eb
Glenn West <glennswest@neuralcloudcomputing.com>
2021-04-09 09:34:30 -0500

    Add repodata

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ export thedir=`pwd | sed 's:/*$::'`
 echo ${thedir}
 go-bin-rpm generate --file rpm.json --version ${theversion} -o esxiredfish-${theversion}.rpm -b ${thedir}/buildarea --arch x86_64
 mv esxiredfish-${theversion}.rpm rpms
-
+(cd rpms;ls -t | tail -n +2 | xargs rm -- )
