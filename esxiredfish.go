304989277294681b76fcabb1d5122a0f4dae52c6
Glenn West <glennswest@neuralcloudcomputing.com>
2021-04-09 12:41:44 -0500

    Update after build

--- esxiredfish.go
+++ esxiredfish.go
@@ -114,7 +114,7 @@ func redfishserver() {
 			"message": "pong",
 		})
 	})
-	r.Run(":80") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run("0.0.0.0:80") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
 
 func IsNumeric(s string) bool {
